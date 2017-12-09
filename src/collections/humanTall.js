fd23c63bcfca395a96846d9d31e6a75ec9e5c955
hectahertz <hectahertz@gmail.com>
2017-12-09 13:52:42 +0100

    Add Tall & Dense weight helpers

--- src/collections/humanTall.js
+++ src/collections/humanTall.js
@@ -1,7 +1,7 @@
 import { Platform } from "react-native";
 import CombinedStyleSheet from "../internal/CombinedStyleSheet";
 import suffixProperties from "../internal/suffixProperties";
-import systemWeights from "../helpers/systemWeights";
+import systemTallWeights from "../helpers/systemTallWeights";
 import human from "./human";
 
 // https://developer.apple.com/videos/play/wwdc2016/801/
@@ -11,47 +11,57 @@ const getStylesForColor = color => {
   return {
     largeTitle: {
       ...human[`largeTitle${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 52
     },
     title1: {
       ...human[`title1${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 43
     },
     title2: {
       ...human[`title2${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 35
     },
     title3: {
       ...human[`title3${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 32
     },
     headline: {
       ...human[`headline${colorSuffix}Object`],
-      ...systemWeights.bold,
+      ...systemTallWeights.semibold,
       lineHeight: 28
     },
     body: {
       ...human[`body${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 28
     },
     callout: {
       ...human[`callout${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 26
     },
     subhead: {
       ...human[`subhead${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 25
     },
     footnote: {
       ...human[`footnote${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 23
     },
     caption1: {
       ...human[`caption1${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 20
     },
     caption2: {
       ...human[`caption2${colorSuffix}Object`],
+      ...systemTallWeights.regular,
       lineHeight: 16
     }
   };
---
+++ src/helpers/systemTallWeights.js
@@ -0,0 +1,30 @@
+import { Platform } from "react-native";
+import sanFranciscoWeights from "../helpers/sanFranciscoWeights";
+import notoTallWeights from "../helpers/notoTallWeights";
+
+// San Francisco jumps from regular to bold starting from semibold for the scripts
+// for which it doesn't have so many weight steps, so let's do the same here.
+
+const systemTallWeights = {
+  thin: Platform.select({
+    android: notoTallWeights.regular,
+    ios: sanFranciscoWeights.thin
+  }),
+  light: Platform.select({
+    android: notoTallWeights.regular,
+    ios: sanFranciscoWeights.light
+  }),
+  regular: Platform.select({
+    android: notoTallWeights.regular,
+    ios: sanFranciscoWeights.regular
+  }),
+  semibold: Platform.select({
+    android: notoTallWeights.bold,
+    ios: sanFranciscoWeights.semibold
+  }),
+  bold: Platform.select({
+    android: notoTallWeights.bold,
+    ios: sanFranciscoWeights.bold
+  })
+};
+export default systemTallWeights;
